12724f5f2471f147a7056967e830e78255e0a1a3
nughuffer <nughuffer2.0@gmail.com>
2017-12-18 23:09:19 -0500

    Update PiSetup.sh

--- PiSetup.sh
+++ PiSetup.sh
@@ -1,29 +1,21 @@
 #!/bin/bash  
+
+# Update the system
 sudo apt-get update 
 sudo apt-get dist-upgrade
 sudo apt-get autoremove
 sudo rpi-update
+# Install ZSH
 sudo apt-get install -y zsh
-
+# Grab and install Oh My ZSH
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)" 
+# Delete zsh config file that Oh my zsh installs automatically
 rm ~/.zshrc 
+# Move our config file to the right directory
 cp zshrc ~/.zshrc
-
-#echo 'alias nano="sudo nano"
-#     alias update="sudo apt-get update && sudo apt-get upgrade && sudo apt-get dist-upgrade && sudo apt-get autoremove && sudo r$
-#     alias aptin="sudo apt-get install -y"
-#     alias apts="sudo apt-cache search"
-#     alias remove="sudo apt-get purge"
-#     alias status="sudo systemctl status"
-#    alias enable="sudo systemctl enable"
-#     alias disable="sudo systemctl disable"
-#     alias restart="sudo systemctl restart"
-#     alias failed="sudo systemctl --failed"
-#     alias reboot="sudo reboot"
-#     alias poweroff="sudo poweroff"' >> ~/.zshrc &&
-
+# Change user shell in system
 chsh -s /bin/zsh 
-
+# Let's change into our new shell
 zsh
 
-echo "Done!"
+echo "Done! You may want to reboot if Raspi FW was updated."
