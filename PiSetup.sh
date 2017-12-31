d3bd6cdc4d5ea5c424d165cf1bee58a595f2a1c9
nughuffer <nughuffer2.0@gmail.com>
2017-12-31 09:44:54 -0500

    Update PiSetup.sh

--- PiSetup.sh
+++ PiSetup.sh
@@ -7,15 +7,19 @@ sudo apt-get autoremove &&
 sudo rpi-update &&
 # Install ZSH
 sudo apt-get install -y zsh &&
-# Grab and install Oh My ZSH
+# Install Screenfetch
+git clone https://github.com/kittykatt/screenFetch &&
+sudo cp screenFetch/screenfetch-dev /usr/bin/screenfetch &&
+cd &&
+sudo rm -R screenFetch &&
+# Install Oh My ZSH
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"  &&
 # Delete zsh config file that Oh my zsh installs automatically
 rm ~/.zshrc &&
 # Move our config file to the right directory
 cp zshrc ~/.zshrc &&
+echo "Type exit to complete setup" && 
 # Change user shell in system
 chsh -s /bin/zsh &&
-# Let's change into our new shell
-zsh &&
 
 echo "Done! You may want to reboot if Raspi FW was updated."
